631c3cfcb343001e77f39d5d5cb53502fe0eadc2
bhowick <bhowickcode@gmail.com>
2015-09-03 21:58:59 -0400

    Made the landing page (index.ejs) slightly less silly.

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
+/*=-=-=-=-=-=-=-=-=-=-=-=-=
+This code is my first attempt at making something semi-meaningful using NodeJS and
+a variety of other helpful code libraries/repositories. I have learned a lot of what's
+in here as I've gone along, and as such, it may appear to be slightly incoherent.
+
+I am improving (hopefully!) as I go along, and will continue to test code here as I 
+am able to learn more of it. 
+
+Currently Learning:
+User Authentication
+
+Learned:
+Database interaction (SQLite)
+Page handling (GET,POST, etc.)
+Parsing variables from a page's body or URL
+Externalizing functions into different files (makes this page look neater!)
+EJS code alongside HTML
+
+=-=-=-=-=-=-=-=-=-=-=-=-=*/
+
+
 var express = require('express'); //Gettin' express code. Express is for NodeJS.
-//THIS IS A COMMENT
-/*THIS IS ALSO, HOPEFULLY*/
-//ALSO IT'S A HAIKU
-var ejs = require('ejs'); //We're going to use the EJS view engine. Whatever that means! This is new to me.
+var ejs = require('ejs'); //We're going to use the EJS view engine to render .ejs pages. Neat, <% %>-escaped stuff.
 var bodyParser = require('body-parser');
 var sqlite3 = require('sqlite3')/*.verbose()*/; //Supposedly .verbose() makes it have more meaningful output to the console.
-var dbFile = './db/db.sqlite';
+var dbFile = './db/db.sqlite'; //This tells us where our database's file is located.
 var db = new sqlite3.Database(dbFile); //Initializes our database as a new object. :)
 var validator = require('validator'); //Helps us validate input! Useful for queries.
 
 //Below this are required modules that we're using, not for node/express. Library directory!
 var indexHandler = require('./lib/indexHandler.js'); //The "." in front of lib is necessary for this! It's not Terminal!
-var addNewEntryHandler = require('./lib/addNewEntryHandler.js'); //Kind of a weird/long name, eh?
-var editEntryHandler = require('./lib/editEntryHandler.js'); //Is there something wrong with this?
+var addNewEntryHandler = require('./lib/addNewEntryHandler.js'); //For adding new database entries.
+var editEntryHandler = require('./lib/editEntryHandler.js'); //For editing database entries.
 
 //Express initialization complete as of the line below.
-var app = express();
+var app = express(); //This tells us to use the "app" object for Express calls.
+
 //BELOW THIS LINE IS WHERE WE DECLARE MIDDLEWARE. It runs every single time before the page loads. 
-app.set('view engine', 'ejs'); //This is a view engine for Express. 
+app.set('view engine', 'ejs'); //This is a view engine for Express. It allows us to parse <% %> code.
 
 app.use(express.static(__dirname + '/views/')); //This tells express to use the /views/ directory.
 app.use(bodyParser.urlencoded({ extended: false })); //This enables bodyParser. I am not totally sure at this time what it does.
@@ -26,8 +45,8 @@ app.use(function (req,res,next) { //Enables us to make database calls and valida
   req.validator = validator;
   next();
 });
-//"it will make sense IN TIME" - Gamemaster
-//uh
+
+//[app.get] - This is used if the server is told to get a URL.
 app.get('/', indexHandler.GET); //Rendering the index page!
 app.get('/viewAll', function(req,res) { //Rendering the database onto a nice little output page!
 	db.all('SELECT * FROM items', function (err,rows) {
@@ -43,9 +62,9 @@ app.get('/viewAll', function(req,res) { //Rendering the database onto a nice lit
 	});
 	
 });
-app.get('/addNewEntry', addNewEntryHandler.GET);
-app.get('/editEntry', editEntryHandler.GET);
-app.get('/confirmDelete', function(req,res) {
+app.get('/addNewEntry', addNewEntryHandler.GET); //Loading the "add new entry" form page.
+app.get('/editEntry', editEntryHandler.GET); //Loading the "edit entry" form page.
+app.get('/confirmDelete', function(req,res) { //Loading the confirmation page for deleting an entry.
 	var id = req.query.id || null;
 	if(id) {
 		db.get('SELECT * FROM items WHERE id = ?', id, function(err,row){
@@ -64,13 +83,13 @@ app.get('/confirmDelete', function(req,res) {
 });
 
 
-//This is used if the server receives a post request! 
-app.post('/', indexHandler.POST);
-app.post('/addNewEntry', addNewEntryHandler.POST);
-app.post('/editEntry', editEntryHandler.POST);
+//[app.post] - This is used if the server receives a post request! 
+app.post('/', indexHandler.POST); //Handling data POSTed to the index page.
+app.post('/addNewEntry', addNewEntryHandler.POST); //Handling data POSTed to the "add new entry" page.
+app.post('/editEntry', editEntryHandler.POST); //Handling data POSTed to the "edit entry" page.
 
-//For handling pages that are only really loaded one way!
-app.all('/deleteEntry', function(req,res) {
+//[app.all] - For handling pages that are only really loaded one way!
+app.all('/deleteEntry', function(req,res) { //Handling database entry deletion and redirecting back to the viewAll page.
 	var id = req.body.id || null;
 	if(id){
 		db.run('DELETE FROM items WHERE id = ?', id, function(err) {
