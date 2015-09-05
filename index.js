c638f32c2ab92534f91c789fe93ac46cab2d0f38
bhowick <bhowickcode@gmail.com>
2015-09-05 02:58:12 -0400

    Learned how to do basic user authentication! Will clean up everything ASAP.

--- index.js
+++ index.js
@@ -22,32 +22,83 @@ EJS code alongside HTML
 /*THIS IS ALSO, HOPEFULLY*/
 //NOW IT'S A HAIKU
 
+//Plugins (is that the right term?)
 var express = require('express'); //Gettin' express code. Express is for NodeJS.
 var ejs = require('ejs'); //We're going to use the EJS view engine to render .ejs pages. Neat, <% %>-escaped stuff.
-var bodyParser = require('body-parser');
-var sqlite3 = require('sqlite3')/*.verbose()*/; //Supposedly .verbose() makes it have more meaningful output to the console.
+var bodyParser = require('body-parser'); //Parsing variables from document bodies.
+var sqlite3 = require('sqlite3').verbose(); //Supposedly .verbose() makes it have more meaningful output to the console.
+
+var validator = require('validator'); //Helps us validate input! Useful for queries.
+var cookieParser = require('cookie-parser'); //For cookies.
+var passport = require('passport'); //For user authentication.
+var session = require('express-session'); //For sessions.
+//var morgan = require('morgan'); //For dev output, apparently. 
+
+
+//Passport (local)
+var LocalStrategy = require('passport-local').Strategy;
+
+
+//SQLite Database
 var dbFile = './db/db.sqlite'; //This tells us where our database's file is located.
 var db = new sqlite3.Database(dbFile); //Initializes our database as a new object. :)
-var validator = require('validator'); //Helps us validate input! Useful for queries.
 
-//Below this are required modules that we're using, not for node/express. Library directory!
+
+//Externalized files to handle certain URLs. Library directory!
 var indexHandler = require('./lib/indexHandler.js'); //The "." in front of lib is necessary for this! It's not Terminal!
 var addNewEntryHandler = require('./lib/addNewEntryHandler.js'); //For adding new database entries.
 var editEntryHandler = require('./lib/editEntryHandler.js'); //For editing database entries.
 
-//Express initialization complete as of the line below.
+
+//Express initialization as "app".
 var app = express(); //This tells us to use the "app" object for Express calls.
+
 
 //BELOW THIS LINE IS WHERE WE DECLARE MIDDLEWARE. It runs every single time before the page loads. 
 app.set('view engine', 'ejs'); //This is a view engine for Express. It allows us to parse <% %> code.
+app.use(session({secret: 'harbfeadtuegwtdlml', resave:true, saveUninitialized:true})); //Not totally sure about these settings.
+//app.use(morgan('dev')); //Not sure.
+app.use(bodyParser.urlencoded({ extended: false })); //This enables bodyParser. 
+app.use(cookieParser('clandestine coin')); //Cookie Parser
+app.use(passport.initialize()); //Initializes passport.
+app.use(passport.session()); //Initializes passport's session.
 
-app.use(express.static(__dirname + '/views/')); //This tells express to use the /views/ directory.
-app.use(bodyParser.urlencoded({ extended: false })); //This enables bodyParser. I am not totally sure at this time what it does.
-app.use(function (req,res,next) { //Enables us to make database calls and validate stuff from other pages.
-  req.db = db;
-  req.validator = validator;
+//Custom page-loading stuff.
+app.use(function (req,res,next) {
+  req.db = db; //Allows us to externalize database calls.
+  req.validator = validator; //Allows us to externalize validator.
   next();
 });
+
+//Local Passport Strategy
+passport.use(new LocalStrategy(
+	function(username, password, done) {
+		db.get('SELECT * FROM users WHERE name = ?', username, function(err,user) { //"user" is the row itself.
+			if(err) { //If the query errors out.
+				return done(err);
+			}
+			if(!user) { //If the user doesn't exist.
+				return done(null, false, { message: 'Username not found.' });
+			}
+			if(!user.pass || user.pass != password) { //If the user's password is empty or doesn't exist.
+				return done(null, false, { message: 'Incorrect password.' });
+			}
+			return done(null, user); //Returns the user as an object if everything checks out.
+		});
+
+	}
+));
+
+//More Passport Stuff
+passport.serializeUser(function (user,done) {
+	done(null,user);
+});
+passport.deserializeUser(function (user,done) {
+	done(null,user);
+});
+
+
+app.use(express.static(__dirname + '/views/')); //This tells express to use the /views/ directory.
 
 //[app.get] - This is used if the server is told to get a URL.
 app.get('/', indexHandler.GET); //Rendering the index page!
@@ -84,12 +135,32 @@ app.get('/confirmDelete', function(req,res) { //Loading the confirmation page fo
 		res.redirect('/viewAll');
 	}
 });
-
+app.get('/login', function (req,res) {
+	res.render('pages/login');
+});
+app.get('/loginFailure', function(req,res) {
+	console.log('Failed to authenticate user.');
+	res.redirect('/login');
+});
+app.get('/loginSuccess', function(req,res) {
+	console.log('Successfully authenticated user.');
+	res.redirect('/');
+});
+app.get('/logout', function (req,res) {
+	req.logout();
+	res.redirect('/');
+});
 
 //[app.post] - This is used if the server receives a post request! 
 app.post('/', indexHandler.POST); //Handling data POSTed to the index page.
 app.post('/addNewEntry', addNewEntryHandler.POST); //Handling data POSTed to the "add new entry" page.
 app.post('/editEntry', editEntryHandler.POST); //Handling data POSTed to the "edit entry" page.
+app.post('/login', passport.authenticate('local', {
+	successRedirect: '/loginSuccess',
+	failureRedirect: '/loginFailure'
+})
+);
+
 
 //[app.all] - For handling pages that are only really loaded one way!
 app.all('/deleteEntry', function(req,res) { //Handling database entry deletion and redirecting back to the viewAll page.
